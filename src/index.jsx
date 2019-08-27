0f56646d19f3ded9daf266531085243c52fb488d
chrys <294931812@qq.com>
2019-08-27 18:25:32 +0800

    add @babel/runtime

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+/*
+* Created by Allen on 2019-08-27
+*/
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Home from './home'
+
+ReactDOM.render(<Home/>, document.querySelector('#root'))
